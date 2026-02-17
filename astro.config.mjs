a89ade788059c103d5a19c74a3de1dab74f3b7a1
mike_banner <mike.webfree@gmail.com>
2026-02-17 03:41:56 +0100

    chore: clean up cloudflare adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  //adapter: node({
-  //mode: 'standalone',
-  //}),
-  adapter: cloudflare(), // Remplace node() par cloudflare()
+  adapter: cloudflare(),
   integrations: [tailwind({
     applyBaseStyles: false,
   })],
